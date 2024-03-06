1261f34e61a70d889c846cb09169e714f9998a62
Pebbles-05 <paul.joydeep04062001@gmail.com>
2024-03-07 00:28:18 +0530

    modified password strength checker function

--- src/components/PassField.svelte
+++ src/components/PassField.svelte
@@ -10,7 +10,8 @@
 		includeNumbers: boolean = true,
 		includeLowercase: boolean = true,
 		includeSpecialChars: boolean = true,
-		isCoppied: boolean = false;
+		isCoppied: boolean = false,
+		passwordStrengthIndicator: passwordStrengthStateType;
 	const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 		lowercaseChars = 'abcdefghijklmnopqrstuvwxyz',
 		numberChars = '0123456789',
@@ -24,7 +25,6 @@
 				isCoppied = true;
 				setTimeout(() => {
 					isCoppied = false;
-					console.log('running');
 				}, 2000);
 				console.log('Copied to clipboard!');
 			})
@@ -44,23 +44,28 @@
 		haveSpecialChars: boolean = includeSpecialChars
 	): void => {
 		let newPassword = '',
-			allChars = '';
+			allChars = '',
+			complexityCount = 0;
 
 		if (haveUppercase) {
 			allChars += uppercaseChars;
 			newPassword += getRandomChar(uppercaseChars);
+			complexityCount++;
 		}
 		if (haveLowercase) {
 			allChars += lowercaseChars;
 			newPassword += getRandomChar(lowercaseChars);
+			complexityCount++;
 		}
 		if (haveNumbers) {
 			allChars += numberChars;
 			newPassword += getRandomChar(numberChars);
+			complexityCount++;
 		}
 		if (haveSpecialChars) {
 			allChars += specialChars;
 			newPassword += getRandomChar(specialChars);
+			complexityCount++;
 		}
 
 		if (allChars === '' && !passwordLength) return;
@@ -74,6 +79,7 @@
 			.split('')
 			.sort(() => Math.random() - 0.5)
 			.join('');
+		checkPasswordStrength(password, complexityCount);
 	};
 
 	$: generatePassword(
@@ -89,35 +95,36 @@
 		const charListSet = new Set(charList);
 		return [...passwordSet].some((letter) => charListSet.has(letter));
 	};
-	const checkPasswordStrength = (password: string): passwordStrengthStateType => {
+	const checkPasswordStrength = (password: string, complexityCount: number = 0) => {
 		const minLength = 8;
 		if (password.length < minLength || commonPasswords.includes(password.toLowerCase())) {
-			return passwordStrengthState.weak;
+			passwordStrengthIndicator = passwordStrengthState.weak;
+			return;
 		}
 
 		const minComplexityCount = 3;
-		let complexityCount = 0;
-		if (checkCommonElements(password, uppercaseChars)) {
-			complexityCount++;
-		}
-		if (checkCommonElements(password, lowercaseChars)) {
-			complexityCount++;
-		}
-		if (checkCommonElements(password, numberChars)) {
-			complexityCount++;
-		}
-		if (checkCommonElements(password, specialChars)) {
-			complexityCount++;
+		if (complexityCount === 0) {
+			if (checkCommonElements(password, uppercaseChars)) {
+				complexityCount++;
+			}
+			if (checkCommonElements(password, lowercaseChars)) {
+				complexityCount++;
+			}
+			if (checkCommonElements(password, numberChars)) {
+				complexityCount++;
+			}
+			if (checkCommonElements(password, specialChars)) {
+				complexityCount++;
+			}
 		}
 
 		if (complexityCount < minComplexityCount && password.length < 16) {
-			return passwordStrengthState.fair;
+			passwordStrengthIndicator = passwordStrengthState.fair;
+			return;
 		}
 
-		return passwordStrengthState.strong;
+		passwordStrengthIndicator = passwordStrengthState.strong;
 	};
-	console.log(commonPasswords);
-	$: passwordStrengthIndicator = checkPasswordStrength(password);
 </script>
 
 <div class="flex flex-col gap-2">
@@ -130,6 +137,7 @@
 			class="w-full bg-transparent outline-none h-max py-4 pr-2 pl-4"
 			type="text"
 			bind:value={password}
+			on:input={() => checkPasswordStrength(password)}
 		/>
 		<div
 			class="cursor-pointer px-2 w-max {isCoppied ? 'pointer-events-none' : ''}"
